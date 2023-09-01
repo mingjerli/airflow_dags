82f4613e30425a2eb04f2ed3684e07438f28623c
None <None>
2023-09-01 16:03:39 +0000

    commit_message as of 2023-09-01T16:03:30.739713

--- fraud_detection_pipeline_module.py
+++ fraud_detection_pipeline_module.py
@@ -45,7 +45,7 @@ def clean_fraud_detection_new_data(rawdata):
 
 
 def split_fraud_detection_data():
-    print("new function as 2023-09-01T13:48:56.383130")
+    print("new function as 2023-09-01T16:03:29.112414")
 
 
 def get_fraud_detection_model(X_train, y_train):
