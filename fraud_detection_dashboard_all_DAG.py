4ee927b54a5de1c91850879cc9718d08a2bdbd12
None <None>
2023-09-01 17:52:54 +0000

    Linea Generated Pipeline Files

--- fraud_detection_dashboard_all_DAG.py
+++ fraud_detection_dashboard_all_DAG.py
@@ -1,15 +1,9 @@
-from airflow import DAG
 import datetime
-from airflow.decorators import task
+
+from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 
-
-default_args = {
-    "owner": "airflow",
-    "depends_on_past": False,
-    "start_date": datetime.datetime(2023, 8, 3),
-}
-
+default_args = {"owner": "airflow", "depends_on_past": False, "start_date": datetime.datetime(2023, 8, 3)}
 with DAG(
     dag_id="fraud_detection_dashboard",
     default_args=default_args,
@@ -18,17 +12,8 @@ with DAG(
     tags=["fraud_detection"],
 ) as dag:
     task_drop_fraud_result_table = PostgresOperator(
-        task_id="drop_fraud_result_table", sql="""DROP TABLE IF EXISTS fraud_results"""
+        task_id="drop_fraud_result_table", sql="DROP TABLE IF EXISTS fraud_results"
     )
-    sql = """
-CREATE TABLE fraud_results AS
-    SELECT fdo.isfraud , fdf.isflaggedfraud, count(1) as count
-    FROM fraud_detection_outcome fdo
-    INNER JOIN fraud_detection_features fdf
-    ON fdo.index=fdf.index
-    Group by fdo.isfraud, rollup(fdf.isflaggedfraud)
-    ORDER by fdf.isflaggedfraud, fdo.isfraud
-"""
+    sql = "\nCREATE TABLE fraud_results AS\n    SELECT fdo.isfraud , fdf.isflaggedfraud, count(1) as count\n    FROM fraud_detection_outcome fdo\n    INNER JOIN fraud_detection_features fdf\n    ON fdo.index=fdf.index\n    Group by fdo.isfraud, rollup(fdf.isflaggedfraud)\n    ORDER by fdf.isflaggedfraud, fdo.isfraud\n"
     task_update_fraud_db = PostgresOperator(task_id="update_fraud_db", sql=sql)
-
     task_drop_fraud_result_table >> task_update_fraud_db
