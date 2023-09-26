3ca6b4e41c1f0366e8769e5f14627dd4e0c7ed64
lineainfra <infra@linea.ai>
2023-09-26 04:55:26 +0000

    Linea Created Commit as of 2023-09-26T04:55:23.438780

---
+++ components/model_training.py
@@ -0,0 +1,22 @@
+def model_training(processed_data):
+    import numpy as np
+    from joblib import dump
+    from sklearn.linear_model import LinearRegression
+
+    x_train_path = processed_data["X_train"]
+    y_train_path = processed_data["y_train"]
+    X_train = np.load(x_train_path)
+    y_train = np.load(y_train_path)
+    model = LinearRegression().fit(X_train, y_train)
+    model_path = "model.joblib"
+    dump(model, model_path)
+    return model_path
+
+
+def model_training_raw(processed_data):
+    from sklearn.linear_model import LinearRegression
+
+    X_train = processed_data["X_train"]
+    y_train = processed_data["y_train"]
+    LinearRegression().fit(X_train, y_train)
+    return model_path
